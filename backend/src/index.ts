eca055176fbaed7f0b97bccca918d72ef6100e17
alansalvaterra <alan.salvaterra@gmail.com>
2024-08-18 15:01:46 -0300

    fix: middleware para habilitar CORS - Cross-Origin Resource Sharing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,34 +2,42 @@ import * as express from "express"
 import * as bodyParser from "body-parser"
 import { Request, Response } from "express"
 import { AppDataSource } from "./data-source"
- import { Routes } from "./routes"
+import { Routes } from "./routes"
 
 AppDataSource.initialize().then(async () => {
 
-   // create express app
-   const app = express()
-   app.use(bodyParser.json())
+    // create express app
+    const app = express()
+    app.use(bodyParser.json())
 
-   // register express routes from defined application routes
-   Routes.forEach(route => {
-       (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-           const result = (new (route.controller as any))[route.action](req, res, next)
-           if (result instanceof Promise) {
-               result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+    // Middleware para habilitar CORS - Cross-Origin Resource Sharing
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
 
-           } else if (result !== null && result !== undefined) {
-               res.json(result)
-           }
-       })
-   })
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next)
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
 
-   // setup express app here
-   // ...
+            } else if (result !== null && result !== undefined) {
+                res.json(result)
+            }
+        })
+    })
 
-   // start express server
-   app.listen(3000)
+    // setup express app here
+    // ...
 
-   console.log("Express server has started on port 3000.")
+    // start express server
+    app.listen(3000)
+
+    console.log("Express server has started on port 3000.")
 
 
 }).catch(error => console.log(error))
