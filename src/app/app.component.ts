381f7d90d7549388073bf244be16dc74af3d3bb7
alansalvaterra <alan.salvaterra@gmail.com>
2024-08-23 14:12:53 -0300

    style: estilo geral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet, Router } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { HomeComponent } from './components/pages/home/home.component';
+import { ButtonComponent } from './components/shared/button/button.component';
+import { NavigationService } from './services/navigation.service';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,17 @@ import { HomeComponent } from './components/pages/home/home.component';
     HeaderComponent, 
     FooterComponent,
     HomeComponent,
+    ButtonComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'praticando-angular';
+
+  constructor(private navigationService: NavigationService) {}
+
+  navigateTo(route: string): void {
+    this.navigationService.navigateTo(route);
+  }
 }
