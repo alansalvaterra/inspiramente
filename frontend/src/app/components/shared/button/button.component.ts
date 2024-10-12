5f0f8d88c585a3af7a298583a8e5778b2cb66d2a
alansalvaterra <alan.salvaterra@gmail.com>
2024-10-12 12:49:27 -0300

    feat: validacao formulario

--- frontend/src/app/components/shared/button/button.component.ts
+++ frontend/src/app/components/shared/button/button.component.ts
@@ -1,18 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
+
 export class ButtonComponent {
   @Input() text: string = 'Botão Padrão';
   @Input() route: string = '/';
+  @Input() disabled: boolean = false; 
   @Output() btnClick = new EventEmitter<string>();
 
   onClick(): void {
-    this.btnClick.emit(this.route);
+    if (!this.disabled) { 
+      this.btnClick.emit(this.route);
+    }
   }
 }
--- frontend/src/app/components/shared/form-pensamento/form-pensamento.component.ts
+++ frontend/src/app/components/shared/form-pensamento/form-pensamento.component.ts
@@ -1,29 +1,57 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { IPensamento } from '../../../interfaces/IPensamento';
 import { CardComponent } from "../thoughts/card/card.component";
 import { PensamentoService } from '../../../services/pensamento.service';
 import { NavigationService } from '../../../services/navigation.service';
 import { Router } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-pensamento',
   standalone: true,
-  imports: [FormsModule, CardComponent, ButtonComponent],
+  imports: [
+    FormsModule, 
+    CardComponent, 
+    ButtonComponent,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './form-pensamento.component.html',
   styleUrl: './form-pensamento.component.css',
 })
+
 export class FormPensamentoComponent {
   @Input() pensamento: IPensamento = { mensagem: '', autor: '', modelo: 0 };
   @Output() onSave = new EventEmitter<IPensamento>();
-  @Output() onCancel = new EventEmitter<void>();
+  // @Output() onCancel = new EventEmitter<void>();
+
+  formulario!: FormGroup;
 
   constructor(
     private navigationService: NavigationService,
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) { }
+
+  ngOnInit(): void {
+    this.formulario = this.fb.group({
+      mensagem: [this.pensamento.mensagem, [Validators.required, Validators.minLength(10), Validators.maxLength(200)]],
+      autor: [this.pensamento.autor, [Validators.required, Validators.minLength(2), Validators.maxLength(20)]],
+      modelo: [this.pensamento.modelo, [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.formulario.valid) {
+      const pensamento: IPensamento = this.formulario.value; 
+      this.salvaPensamento(pensamento); 
+    } else {
+      this.formulario.markAllAsTouched();
+    }
+  }
 
   salvaPensamento(pensamento: IPensamento): void {
     this.service.salvaPensamento(pensamento).subscribe(() => {
