b16c357b8bccecb698f87696a455321c477ff7b1
alansalvaterra <alan.salvaterra@gmail.com>
2024-10-12 12:58:56 -0300

    fix: bug na previa ao editar pensamento

--- frontend/src/app/components/pages/editar-pensamento/editar-pensamento.component.ts
+++ frontend/src/app/components/pages/editar-pensamento/editar-pensamento.component.ts
@@ -1,23 +1,22 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { PensamentoService } from '../../../services/pensamento.service';
 import { NavigationService } from '../../../services/navigation.service';
 import { IPensamento } from '../../../interfaces/IPensamento';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormPensamentoComponent } from '../../shared/form-pensamento/form-pensamento.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-editar-pensamento',
   standalone: true,
   imports: [
-    FormsModule,
-    FormPensamentoComponent
+    FormPensamentoComponent,
+    CommonModule
   ],
   templateUrl: './editar-pensamento.component.html',
   styleUrl: './editar-pensamento.component.css'
 })
 export class EditarPensamentoComponent {
-
   pensamento: IPensamento = {
     id: 0,
     mensagem: '',
@@ -29,21 +28,24 @@ export class EditarPensamentoComponent {
     private navigationService: NavigationService,
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id')
-    this.service.listarPorId(parseInt(id!)).subscribe((pensamento) => {
-      this.pensamento = pensamento
-    })
-  }
-
-  editaPensamento(): void {
-      this.service.editaPensamento(this.pensamento).subscribe(() => {
-        alert('Pensamento editado com sucesso!');
-        this.router.navigate(['/home'])
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.service.listarPorId(parseInt(id!, 10)).subscribe((pensamento) => {
+        this.pensamento = pensamento;
       });
+    }
+  }
+  
+
+  editaPensamento(pensamento: IPensamento): void {
+    this.service.editaPensamento(pensamento).subscribe(() => {
+      alert('Pensamento editado com sucesso!');
+      this.router.navigate(['/home']);
+    });
   }
 
   navigateTo(route: string): void {
